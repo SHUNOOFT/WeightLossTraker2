5254b78edadb9b62afac8483cb1cd9bd34bea440
Shun Ooft <shunooft19971008@gmail.com>
2023-12-10 18:56:45 +0900

    chart(js)の導入

--- app/controllers/progress_charts_controller.rb
+++ app/controllers/progress_charts_controller.rb
@@ -1,5 +1,9 @@
 class ProgressChartsController < ApplicationController
   def index
+    # この部分で、適切な ProgressChart レコードを取得
+    @progress_chart = ProgressChart.find_by(user_id: params[:user_id]) # または他の方法で適切なレコードを取得
 
+    # JSON 形式でデータを設定
+    @progress_data_json = @progress_chart&.data.to_json
   end
 end
