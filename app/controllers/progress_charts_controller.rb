f7e0c49c12ee8243e725b36ccbd991131f52fa0e
Shun Ooft <shunooft19971008@gmail.com>
2023-12-11 14:40:32 +0900

    chart(js)完

--- app/controllers/progress_charts_controller.rb
+++ app/controllers/progress_charts_controller.rb
@@ -5,13 +5,14 @@ class ProgressChartsController < ApplicationController
     user = daily_weight.user
 
     # user に関連する ProgressChart を取得
-    @progress_chart = ProgressChart.find_by(user: user)
+    @progress_chart = user.progress_chart
 
-    # チャートデータの初期化
+    # チャートデータの初期化とy軸の範囲の初期化
     @progress_chart_data = {}
+    @y_min = 0
+    @y_max = 100
 
-    # プログレスチャートが存在する場合のみデータ変換
-    if @progress_chart
+    if @progress_chart && @progress_chart.data.any?
       one_month_ago = Date.today - 1.month
       chart_data = @progress_chart.data.select { |entry| Date.parse(entry['date']) >= one_month_ago }
 
@@ -19,6 +20,12 @@ class ProgressChartsController < ApplicationController
       @progress_chart_data = chart_data.each_with_object({}) do |entry, hash|
         hash[entry['date']] = entry['weight'].to_f
       end
+
+      # 最新の体重を取得してグラフの範囲を設定
+      latest_weight = chart_data.map { |d| d['weight'].to_f }.max
+      weight_range = 5 # 体重の変動幅 +/- 5kg
+      @y_min = latest_weight - weight_range
+      @y_max = latest_weight + weight_range
     end
   end
 end
